ce93bc4bec2ddb1c6f39c55b3e899f32f96cff81
jayrgee <231877+jayrgee@users.noreply.github.com>
2024-07-01 14:30:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-generics-simple-deborahk-cdzu29
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jayrgee/stackblitz-generics-simple-deborahk-cdzu29)
---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import { Component, signal } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <div>Snack: {{ snack().name }} In stock: {{snack().isInStock}}</div>
+    <div><button (click)='updateSnack()'>Update Snack</button></div>
+    <br/>
+    <div>User: {{ user().name }}</div>
+    <div><button (click)='updateUser()'>Update User</button></div>
+  `,
+})
+export class App {
+
+  snack = signal<Snack>(SNACK);
+  user = signal<User>(USER);
+
+  updateSnack() {
+    this.snack.update(s => ({
+      ...s,
+      name: 'peanuts'
+    }));
+    console.log(this.snack());
+  }
+
+  updateUser() {
+    this.user.update(u => ({
+      ...u,
+      name: 'Frodo'
+    }));
+    console.log(this.user());
+  }
+
+}
+
+bootstrapApplication(App);
+
+export const SNACK: Snack = { id: 1, name: 'popcorn', price: 2.0, isInStock: true };
+export const USER: User = { id: 5, name: 'Bilbo', userName: 'Hobbit1' };
+
+export interface Snack {
+  id: number;
+  name: string;
+  price: number;
+  isInStock: boolean;
+}
+export interface User {
+  id: number;
+  name: string;
+  userName: string;
+}
